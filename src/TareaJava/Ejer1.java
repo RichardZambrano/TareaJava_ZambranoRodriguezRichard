979365932e1ab2cb985c386ed2e0a577d7ff97fa
Richard Zambrano <x_richard_91@hotmail.com>
2014-07-02 00:12:18 -0500

    tercero Commit

--- src/TareaJava/Ejer1.java
+++ src/TareaJava/Ejer1.java
@@ -4,6 +4,19 @@
  * and open the template in the editor.
  */
 
+/*
+*1.- Mira esta serie: 7, 6, 8, 4, 9, 2, 10, 0, 11, -2, ...
+*Cree una función que recibe dos enteros: x e y. Si alguno de ellos es 0 o negativo,
+* si son mayores que 255, la función debe devolver -1
+*De lo contrario, la función debe devolver la suma de los elementos X e Y de la serie.
+*Por ejemplo: Si la función recibe x = 1, y = 3, se debería devolver: 
+*15 (Debido a que la suma de la primera, más el tercer argumento es 7 +8 = 15)..
+*Si la función recibe x = 8, y = 9, es conveniente devolver 11.
+*(Debido a que la suma de la octava más el elemento noveno es 0 11 = 11).
+*La función recibirá 2 enteros, y devuelve un entero.
+
+*/
+
 package TareaJava;
 import javax.swing.JOptionPane;
 /**
