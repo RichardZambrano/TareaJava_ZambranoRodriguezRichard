eda369837da0b9f1f7826508ce535d9068334fe2
Richard Zambrano <x_richard_91@hotmail.com>
2014-07-02 00:28:19 -0500

    Commit

--- src/TareaJava/Ejer3.java
+++ src/TareaJava/Ejer3.java
@@ -4,6 +4,17 @@
  * and open the template in the editor.
  */
 
+/*
+
+3. Mira esta serie: 60, 30, 20, 15, 12 ... la semilla de esta serie fue el número 60.
+Cree una función que recibe dos enteros: x, y y. Si alguno de ellos es 0 o negativo, o si son mayores que 255, 
+la función debe devolver -1.
+La función debe devolver el elemento y de las series generadas por x.
+Por ejemplo: Si la función recibe x = 60, y = 3, devolverá 20, porque el 20 es el elemento 3 º 
+en la serie genera cuando x = 60.
+La función recibirá 2 enteros, devuelve un valor de punto flotante.
+
+*/
 package TareaJava;
 
 import javax.swing.JOptionPane;
