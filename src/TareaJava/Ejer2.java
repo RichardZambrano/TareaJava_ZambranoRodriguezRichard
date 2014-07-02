7a38b2b81dda79f78d777d57e2ffb7aa1bfbe4da
Richard Zambrano <x_richard_91@hotmail.com>
2014-07-02 00:24:00 -0500

    Commit

--- src/TareaJava/Ejer2.java
+++ src/TareaJava/Ejer2.java
@@ -5,10 +5,15 @@
  */
 /*
 2.) Mira esta serie: 2, 2, 4, 12, 48, ... 
-la semilla de esta serie fue el número 2 Mira esta serie:. 3, 3, 6, 18, 72, ... la semilla de esta serie fue el número 3.
-Cree una función que recibe dos enteros: x, y  y. Si alguno de ellos es 0 o negativo, o si son mayores que 255, la función debe devolver -1
+la semilla de esta serie fue el número 2 Mira esta serie:. 3, 3, 6, 18, 72, ... 
+la semilla de esta serie fue el número 3.
+Cree una función que recibe dos enteros: x, y  y. 
+Si alguno de ellos es 0 o negativo, o si son mayores que 255,
+la función debe devolver -1
 La función debe devolver el elemento y de las series generadas por x.
-Por ejemplo, si la serie recibe x = 3, y = 4, es conveniente devolver 72, porque 72 es el cuarto elemento de la serie generado cuando x = 3.
+Por ejemplo, si la serie recibe x = 3, y = 4,
+es conveniente devolver 72, porque 72 es el cuarto elemento de la serie
+generado cuando x = 3.
 La función recibirá 2 enteros, y devuelve un entero.
 
 */
